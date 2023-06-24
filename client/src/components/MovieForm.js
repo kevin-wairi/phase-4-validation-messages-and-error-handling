f61b23c09ed7e3d0d581f8dbe31de65ddb3cd31c
kevin-wairi <kevinwairi@gmail.com>
2023-06-24 17:44:15 +0300

    assignment completed

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -15,23 +15,25 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/movies", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-    .then((response) => {
-      if (response.ok) {
-        response.json().then((newMovie) => console.log(newMovie));
-      } else {
-        response.json().then((errorData) => setErrors(errorData.errors));
-      }
-    })
+  // make the function async to enable the await keyword
+async function handleSubmit(e) {
+  e.preventDefault();
+  // fetch returns a Promise, we must await it
+  const response = await fetch("/movies", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  // response.json() returns a Promise, we must await it
+  const data = await response.json();
+  if (response.ok) {
+    console.log("Movie created:", data);
+  } else {
+    setErrors(data.errors);
   }
+}
 
   function handleChange(e) {
     const value =
